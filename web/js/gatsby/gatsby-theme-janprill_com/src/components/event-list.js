514d0fe39bed5d453286cb6f1ab9d853db93d4ff
Jan Prill <Jan.Prill@mspag.com>
2019-08-19 10:19:02 +0200

    Learning gatsby theme development - especially components

---
+++ web/js/gatsby/gatsby-theme-janprill_com/src/components/event-list.js
@@ -0,0 +1,3 @@
+import React from 'react'
+const EventList = ({ events }) => <pre>{JSON.stringify(events, null, 2)}</pre>
+export default EventList
---
+++ web/js/gatsby/gatsby-theme-janprill_com/src/components/layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+const Layout = ({ children }) => (
+  <div>
+    <h1>Gatsby Events</h1>
+    {children}
+  </div>
+)
+
+export default Layout
