da012697fd36d6b6d2ed2fb572bace8886f81768
Jan Prill <Jan.Prill@mspag.com>
2019-11-12 08:12:13 +0100

    wip: home page

---
+++ web/js/gridsome/portfolio/src/layouts/Inviado.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="m-0">
+    <header class="m-0 bg-black p-3">
+      <div class="font-copy text-white text-l">
+       INVIADO
+      </div>
+    </header>
+
+    <main>
+      <slot />
+    </main>
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: [],
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen
+    },
+  },
+}
+</script>
+
+<style src="../css/main.css" />
+<link rel="stylesheet" href="https://use.typekit.net/qqr0xyz.css">
---
+++ web/js/gridsome/portfolio/src/pages/Inviado.vue
@@ -0,0 +1,41 @@
+<template>
+  <Layout>
+    <div class="m-7 bg-black pt-6">
+      <div class="max-w-4xl m-auto leading-tight">
+        <h1 class="text-center font-copy text-5xl text-gray-100">
+          Inviado hilft Dir, als DEV Du selbst zu sein.
+        </h1>
+      </div>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query Documentation {
+  docs: allDocumentation (sortBy: "date", order: DESC) {
+    edges {
+      node {
+        id
+        path
+        title
+        excerpt
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import Layout from '~/layouts/Inviado'
+
+export default {
+  metaInfo: {
+    title: 'Docs'
+  },
+  components: {
+     Layout,
+  },
+
+}
+
+</script>
