e2411d5d3da7e2873914554e4b93aa52e85617f5
Jan Prill <Jan.Prill@mspag.com>
2019-11-29 18:33:55 +0100

    wip

--- web/js/gridsome/inviado/src/components/CallToAction.vue
+++ web/js/gridsome/inviado/src/components/CallToAction.vue
@@ -1,24 +1,6 @@
 <template>
 	<section class="antialiased font-copy w-full bg-black text-left text-white">
 
-		<!--
-		<div class="container mx-auto font-normal leading-normal">
-			<h3 class="font-neuzeit font-bold text-2xl md:text-3xl lg:text-4xl">Lade Deinen kostenlosen Report herunter</h3>
-			<div class="mt-4 sm:flex">
-				<input type="email" class="block w-full focus:outline-0 bg-white py-3 px-6 mb-2 sm:mb-0 text-2xl font-neuzeit" name="email" placeholder="Enter your email" required="">
-				<button class="h-16 uppercase text-md text-white font-bold focus:outline-0 w-full sm:w-auto bg-red-700 hover:bg-red-400 focus:bg-blue px-6">
-					Ja, schickt mir jetzt 5 Wege, mein Gehalt zu verdoppeln.
-				</button>
-			</div>
-			<div class="text-md leading-none mt-4">
-				Inviado sendet Dir Deinen kostenlosen Report nach Anmeldung an unseren Newsletter beim bekannten Anbieter <em>Mailchimp</em>.
-				Du willigst ein, diesen Newsletter zu abonnieren. Du kannst Dich jederzeit wieder abmelden.
-				Von der Einwilligung mitumfasst ist eine Erfolgsmessung. Weitere Hinweise erhalten Sie in unserer 
-				<a class="underline" href="/datenschutz">Datenschutzerklärung</a>
-			</div>
-		</div>
-		-->
-
 		<div>
 			<!-- Begin Mailchimp Signup Form -->
 			<div id="mc_embed_signup" class="container mx-auto font-normal leading-normal">
@@ -31,7 +13,7 @@
 									<label class="checkbox subfield text-white" for="gdpr_15943">
 										<ValidationProvider name="Zustimmung" :rules="{ required: { allowFalse: false } }" v-slot="{ errors }">
 											<input type="checkbox" id="gdpr_15943" name="gdpr[15943]" value="Y" class="form-checkbox text-green-600 h-6 w-6 text-xl" v-model="agree" />
-											<span class="ml-2">Ja, ich möchte den kostenlosen Report erhalten und willige ein, Emails zu erhalten.* (Pflichtfeld)</span>
+											<span class="ml-2">Ja, ich möchte den kostenlosen Report herunterladen und willige ein, Emails zu erhalten.* (Pflichtfeld)</span>
 											<span class="text-red-500 border-red-500 ml-4">{{ errors[0] }}</span>
 										</ValidationProvider>
 									</label>
@@ -55,7 +37,7 @@
 								Inviado sendet Dir Deinen kostenlosen Report nach Anmeldung an unseren Newsletter. Er wird vom bekannten Anbieter <em>Mailchimp</em> gehostet.
 								Du willigst ein, diesen Newsletter zu abonnieren. Du kannst Dich jederzeit wieder abmelden.
 								Von der Einwilligung mitumfasst ist eine Erfolgsmessung. Weitere Hinweise erhalten Sie in unserer 
-								<a class="underline" href="/datenschutz">Datenschutzerklärung</a>
+								<a class="underline" href="/datenschutz" target="_blank">Datenschutzerklärung</a>
 							</div>
 
 							<div id="mce-responses" class="clearfix text-white">
