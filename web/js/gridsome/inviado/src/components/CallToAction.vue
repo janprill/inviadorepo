6cbaefb9d2176bd40f147f446e0bac5e3a4a084f
Jan Prill <Jan.Prill@mspag.com>
2019-11-29 15:28:57 +0100

    validation (wip)

--- web/js/gridsome/inviado/src/components/CallToAction.vue
+++ web/js/gridsome/inviado/src/components/CallToAction.vue
@@ -1,6 +1,7 @@
 <template>
 	<section class="antialiased font-copy w-full bg-black text-left text-white">
 
+		<!--
 		<div class="container mx-auto font-normal leading-normal">
 			<h3 class="font-neuzeit font-bold text-2xl md:text-3xl lg:text-4xl">Lade Deinen kostenlosen Report herunter</h3>
 			<div class="mt-4 sm:flex">
@@ -16,50 +17,56 @@
 				<a class="underline" href="/datenschutz">Datenschutzerklärung</a>
 			</div>
 		</div>
+		-->
 
 		<div>
 			<!-- Begin Mailchimp Signup Form -->
 			<div id="mc_embed_signup" class="container mx-auto font-normal leading-normal">
 				<h3 class="font-neuzeit font-bold text-2xl md:text-3xl lg:text-4xl">Lade Deinen kostenlosen Report herunter</h3>
-				<form action="https://inviado.us5.list-manage.com/subscribe/post?u=39c6fa54f93cbe29ec0d797ed&amp;id=0f0dfbd450" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-					<div id="mc_embed_signup_scroll">
-						<div class="mt-4 sm:flex">
-							<input type="email" id="mce-EMAIL" v-model="email" class="block w-full focus:outline-0 bg-white py-3 px-6 mb-2 sm:mb-0 text-2xl font-neuzeit text-gray-900" name="EMAIL" placeholder="Deine Email*" required="*">
-							<button type="submit" 
-											name="subscribe" id="mc-embedded-subscribe" v-bind:disabled="isSubscribeDisabled"
-											data-tooltip="Insert email and confirm that you agree" 
-											class="h-16 uppercase text-md text-white font-bold focus:outline-0 w-full sm:w-auto bg-red-700 hover:bg-red-400 focus:bg-blue px-6">
-								Ja, schickt mir jetzt 5 Wege, mein Gehalt zu verdoppeln.
-							</button>
-						</div>
-						<div class="text-md leading-none mt-4">
-							Inviado sendet Dir Deinen kostenlosen Report nach Anmeldung an unseren Newsletter beim bekannten Anbieter <em>Mailchimp</em>.
-							Du willigst ein, diesen Newsletter zu abonnieren. Du kannst Dich jederzeit wieder abmelden.
-							Von der Einwilligung mitumfasst ist eine Erfolgsmessung. Weitere Hinweise erhalten Sie in unserer 
-							<a class="underline" href="/datenschutz">Datenschutzerklärung</a>
-						</div>
-
-						<div id="mergeRow-gdpr" class="mergeRow gdpr-mergeRow content__gdprBlock mc-field-group">
-							<div class="content__gdpr">
-								<p>Ja, ich möchte mein kostenlosen Report erhalten und willige ein, Emails von Inviado zu erhalten.</p>
-								<fieldset class="mc_fieldset gdprRequired mc-field-group" name="interestgroup_field">
-									<label class="checkbox subfield" for="gdpr_15943">
-										<input type="checkbox" id="gdpr_15943" name="gdpr[15943]" value="Y" class="av-checkbox " v-model="agree" />
+				<ValidationObserver v-slot="{ invalid }">
+					<form action="https://inviado.us5.list-manage.com/subscribe/post?u=39c6fa54f93cbe29ec0d797ed&amp;id=0f0dfbd450" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+						<div id="mc_embed_signup_scroll">
+							<div id="mergeRow-gdpr" class="mergeRow gdpr-mergeRow content__gdprBlock mc-field-group">
+								<div class="content__gdpr">
+									<label class="checkbox subfield text-white" for="gdpr_15943">
+										<ValidationProvider name="Zustimmung" :rules="{ required: { allowFalse: false } }" v-slot="{ errors }">
+											<input type="checkbox" id="gdpr_15943" name="gdpr[15943]" value="Y" class="form-checkbox text-green-600 h-6 w-6 text-xl" v-model="agree" />
+											<span class="ml-2">Ja, ich möchte den kostenlosen Report erhalten und willige ein, Emails zu erhalten.</span>
+											<span class="text-red-500 border-red-500">{{ errors[0] }}</span>
+										</ValidationProvider>
 									</label>
-								</fieldset>
+								</div>
 							</div>
-							<div class="content__gdprLegal">
-								<p>We use Mailchimp as our marketing platform. By clicking below to subscribe, you acknowledge that your information will be transferred to Mailchimp for processing. <a href="https://mailchimp.com/legal/" target="_blank">Learn more about Mailchimp's privacy practices here.</a></p>
+
+							<div class="mt-4 sm:flex">
+								<ValidationProvider name="E-mail" rules="required|email" v-slot="{ errors }">
+									<input type="email" id="mce-EMAIL" v-model="email" class="block w-full focus:outline-0 bg-white py-3 px-6 mb-2 sm:mb-0 text-2xl font-neuzeit text-gray-900" name="EMAIL" placeholder="Deine Email*" required="*">
+									<span>{{ errors[0] }}</span>
+								</ValidationProvider>
+								<button type="submit" 
+												name="subscribe" id="mc-embedded-subscribe" 
+												:disabled="invalid"
+												data-tooltip="Insert email and confirm that you agree" 
+												class="h-16 uppercase text-md text-white font-bold focus:outline-0 w-full sm:w-auto bg-red-700 hover:bg-red-400 focus:bg-blue px-6">
+									Ja, schickt mir jetzt 5 Wege, mein Gehalt zu verdoppeln.
+								</button>
 							</div>
+							<div class="text-md leading-none mt-4">
+								Inviado sendet Dir Deinen kostenlosen Report nach Anmeldung an unseren Newsletter beim bekannten Anbieter <em>Mailchimp</em>.
+								Du willigst ein, diesen Newsletter zu abonnieren. Du kannst Dich jederzeit wieder abmelden.
+								Von der Einwilligung mitumfasst ist eine Erfolgsmessung. Weitere Hinweise erhalten Sie in unserer 
+								<a class="underline" href="/datenschutz">Datenschutzerklärung</a>
+							</div>
+
+							<div id="mce-responses" class="clearfix text-white">
+								<div class="response" id="mce-error-response" style="display:none"></div>
+								<div class="response" id="mce-success-response" style="display:none"></div>
+							</div>    
+							<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+							<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_39c6fa54f93cbe29ec0d797ed_0f0dfbd450" tabindex="-1" value=""></div>
 						</div>
-						<div id="mce-responses" class="clear">
-							<div class="response" id="mce-error-response" style="display:none"></div>
-							<div class="response" id="mce-success-response" style="display:none"></div>
-						</div>    
-						<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-						<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_39c6fa54f93cbe29ec0d797ed_0f0dfbd450" tabindex="-1" value=""></div>
-					</div>
-				</form>
+					</form>
+				</ValidationObserver>
 			</div>
 			<!--End mc_embed_signup-->
 		</div>
@@ -81,9 +88,19 @@ export default {
   },
   computed: {
     isSubscribeDisabled: function() {
-      var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,50}/igm
-      return !(this.agree && re.test(this.email))
-    }
-  }
+      let re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,50}/igm
+			let valid = (this.agree && re.test(this.email))
+      return !valid;
+    },
+	},
+	methods: {
+		buttonClick: function(event) {
+			let re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,50}/igm
+			let valid = (this.agree && re.test(this.email))
+			if (!valid) {
+				alert("Bitte gib eine gültige Emailadresse ein und bestätige die Checkbox")
+			}
+		}
+	}
 }
 </script>
--- web/js/gridsome/inviado/src/main.js
+++ web/js/gridsome/inviado/src/main.js
@@ -1,7 +1,12 @@
 import DefaultLayout from '~/layouts/Default.vue'
+import { ValidationObserver, ValidationProvider, extend, localize } from 'vee-validate';
+import { required, email } from 'vee-validate/dist/rules';
+import de from 'vee-validate/dist/locale/de.json';
 
 export default function (Vue, { head }) {
   Vue.component('Layout', DefaultLayout)
+  Vue.component('ValidationObserver', ValidationObserver);
+  Vue.component('ValidationProvider', ValidationProvider);
 
   head.htmlAttrs = { lang: 'en', class: 'h-full' }
   head.bodyAttrs = { class: 'antialiased font-serif' }
@@ -10,6 +15,13 @@ export default function (Vue, { head }) {
     rel: 'stylesheet',
     href: 'https://use.typekit.net/qqr0xyz.css'
   })
+
+  extend('email', email)
+  extend('required', required)
+
+  localize('de', de)
+
+
 
 
 }
