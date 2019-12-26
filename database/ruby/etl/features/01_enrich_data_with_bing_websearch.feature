502bca3da276d3ae8b6f441d312964dcc4c209b7
Jan Prill <Jan.Prill@mspag.com>
2019-12-26 21:38:54 +0100

    learning a little bit of cucumber along the way. aiming at json parsing of the bing result as a first test

---
+++ database/ruby/etl/features/01_enrich_data_with_bing_websearch.feature
@@ -0,0 +1,7 @@
+Feature: Parse bing websearch results
+  As a prerequisite of enriching data via bing websearch result we need to be able to parse the results bing is responding with.
+
+  Scenario: bing responded with a json result
+    Given bing responded with a json answer
+    When I parse this json
+    Then I should be able to extract the first result
---
+++ database/ruby/etl/features/step_definitions/01_bing.rb
@@ -0,0 +1,11 @@
+Given("bing responded with a json answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("I parse this json") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("I should be able to extract the first result") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
