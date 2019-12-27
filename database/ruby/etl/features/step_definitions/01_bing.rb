28d5bba5e166b54a53d6db58b7951b42a6e3df3a
Jan Prill <Jan.Prill@mspag.com>
2019-12-27 07:59:52 +0100

    learning a little bit of cucumber along the way. aiming at json parsing of the bing result as a first test

--- database/ruby/etl/features/step_definitions/01_bing.rb
+++ database/ruby/etl/features/step_definitions/01_bing.rb
@@ -1,5 +1,9 @@
 Given("bing responded with a json answer") do
-  pending # Write code here that turns the phrase above into concrete actions
+  file = File.open("#{__dir__}/../resources/test.json").read
+  file.each_line do |line|
+    p "#{line}"
+  end
+
 end
 
 When("I parse this json") do
