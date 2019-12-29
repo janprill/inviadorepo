9509810808c564a424b7ba7a4713565f205e2ba3
Jan Prill <Jan.Prill@mspag.com>
2019-12-29 19:11:11 +0100

    edit json in activeadmin

--- rails/inviado-on-rails/app/models/concerns/membershipable.rb
+++ rails/inviado-on-rails/app/models/concerns/membershipable.rb
@@ -5,11 +5,5 @@ module Membershipable
     has_many :members, through: :memberships
 
     accepts_nested_attributes_for :members
-
-    # after_create :create_membership
-  end
-
-  def create_membership
-    Membership.create(membershipable_type: self.class.name, membershipable_id: self.id, member_id: self.member_id)
   end
 end
