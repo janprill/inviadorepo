04a08769eac3dbf30c9f91c673700037d278ab72
Jan Prill <Jan.Prill@mspag.com>
2019-12-31 23:43:37 +0100

    Here I might place service objects

---
+++ rails/inviado-on-rails/app/services/test_service.rb
@@ -0,0 +1,15 @@
+module Inviado
+  module Services
+    class Test 
+      def moin
+        p '-------------------------------moin' 
+      end
+    end
+  end
+end
+
+class TestService
+      def moin
+        p '-------------------------------moin' 
+      end
+end
