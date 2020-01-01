ff41152a49fe85d38dd29f03e95d5b200298118b
Jan Prill <Jan.Prill@mspag.com>
2020-01-01 15:05:34 +0100

    wip: cucumber specs to parse bing websearch results (pending)

--- rails/inviado-on-rails/features/10_data_enhancement_with_bing.feature
+++ rails/inviado-on-rails/features/10_data_enhancement_with_bing.feature
@@ -4,10 +4,23 @@
 Feature: Enhance data via bing websearches
 
   Scenario: Retrieve data from the db and construct queries
-    Given data (here: organizations) retrievable from the db
-    When an organization has the name of {Inviado Ltd & Co KG}
+    Given data, here: organizations, retrievable from the db
+    When an organization has the name of "Inviado Ltd & Co KG"
     But no site-url is known
-    Then a simple query should be constructed {todo}
+    Then a simple query should be constructed "todo"
 
-    When there is a name like {Inviado Ltd & Co KG} and a site like {inviado.com}
-    Then the query should be site-scoped like {todo}
+    When there is a name like "Inviado Ltd & Co KG" and a site like "inviado.com"
+    Then the query should be site-scoped like "todo"
+
+  Scenario: Query the bing websearch api
+    Given an organization name
+    When the websearch api is queried
+    Then bing answeres with a json response that is parseable
+
+  Scenario: Parsing the result
+    Given a json answer
+    When the json is parsed
+    Then a url and a description is extractable
+
+    When the url and description are extracted
+    Then this information is persisted as a link to enhance the data
---
+++ rails/inviado-on-rails/features/step_definitions/10_bing_enhancement.rb
@@ -0,0 +1,55 @@
+Given("data, here: organizations, retrievable from the db") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("an organization has the name of {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("no site-url is known") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("a simple query should be constructed {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("there is a name like {string} and a site like {string}") do |orgname, website|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("the query should be site-scoped like {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("an organization name") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("the websearch api is queried") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("bing answeres with a json response that is parseable") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given("a json answer") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("the json is parsed") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("a url and a description is extractable") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When("the url and description are extracted") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then("this information is persisted as a link to enhance the data") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
