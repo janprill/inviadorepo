3c250b3671ec9a2b83644a73c4173ea9f359b750
Jan Prill <Jan.Prill@mspag.com>
2020-01-17 17:53:21 +0100

    svg animation experiment

--- web/js/gridsome/inviado/src/components/PurpleCard.vue
+++ web/js/gridsome/inviado/src/components/PurpleCard.vue
@@ -25,16 +25,23 @@
           What you might not yet know...
         </div>
       </section>
-      <section class="m-3 pb-4 text-gray-100">
+      <section class="m-3 pb-4 text-gray-100 flex flex-wrap">
         <font-awesome :icon="['fab', 'github']" class="text-white h-5 w-5 shadow-lg m-1 rounded"/>
         <font-awesome :icon="['fab', 'xing-square']" class="text-white h-5 w-5 shadow-lg m-1 rounded"/>
+      </section>
+      <section>
+        <More />
       </section>
     </div>
   </div>
 </template>
 
 <script>
+import More from '@/components/More'
 export default {
   props: ['index', 'property'],
+  components: {
+    More
+  }
 }
 </script>
