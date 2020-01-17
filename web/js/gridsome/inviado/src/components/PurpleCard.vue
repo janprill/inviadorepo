1d37536e2d5e9e8df647f3cb1e9431072ca6f51f
Jan Prill <Jan.Prill@mspag.com>
2020-01-17 13:02:45 +0100

    Purple Card

---
+++ web/js/gridsome/inviado/src/components/PurpleCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="p-2 pl-8 pt-12 h-auto">
+    <div class="">
+      
+    </div>
+    <div class="shadow shadow-primary pt-8">
+      <section class="flex -ml-6">
+        <div class="flex-shrink bg-gray-200">
+          img
+        </div>
+        <div class="flex-grow bg-gray-200">
+          text
+        </div>
+      </section>
+      <section class="mt-4 p-2 text-gray-100">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, perspiciatis! Qui rem iusto aliquid ut, expedita culpa nemo amet beatae libero at ipsam ex quasi quis! Atque neque libero inventore.
+      </section>
+      <section class="m-3 shadow-inner p-2 bg-gray-900 rounded text-gray-100">
+        Psst
+      </section>
+      <section class="m-3 pb-4 text-gray-100">
+        XING et al
+      </section>
+    </div>
+  </div>
+</template>
---
+++ web/js/gridsome/inviado/src/pages/Purple.vue
@@ -0,0 +1,23 @@
+<template>
+  <Layout class="bg-primary">
+    <main class="container mx-auto">
+      <div class="min-w-full">
+        <div class="sm:mt-0 sm:w-80 sm:flex-shrink-0 lg:flex lg:flex-wrap">
+          <div v-for="index in 10" :key="index" class="lg:w-1/3">
+            <PurpleCard :index=index />
+          </div>
+        </div>
+      </div>
+    </main>
+  </Layout>
+</template>
+
+<script>
+import PurpleCard from '@/components/PurpleCard'
+
+export default {
+  components: {
+    PurpleCard
+  }
+}
+</script>
