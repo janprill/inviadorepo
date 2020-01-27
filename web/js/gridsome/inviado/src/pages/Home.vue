f5d2d11daca04c089180958b3b2c7d7d6730ae7a
Jan Prill <Jan.Prill@mspag.com>
2020-01-27 22:40:57 +0100

    the home (wip)

---
+++ web/js/gridsome/inviado/src/pages/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <Layout>
+    <main class="font-neuzeit text-white container mx-auto">
+      <section class="flex items-end">
+        <section class="mt-32 text-5xl font-hairline leading-tight">
+          <h1>
+            We help you to earn so 
+            much more money
+            with your IT career in 
+            Germany that it almost 
+            feels like an illegal
+            hack.
+          </h1>
+        </section>
+        <section class="ml-24 italic">
+          An outstanding career consists of a series of right decisions. 
+          Starting with choosing the right employer at the right time. 
+          With our career due dilligince we help you to make the right 
+          decisions and avoid the wrong ones. And here is the hack: 
+          It is not you who pays for this service, but the employers in your career do.
+        </section>
+      </section>
+    </main>
+  </Layout>
+</template>
