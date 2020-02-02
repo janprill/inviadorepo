c41e14c8c854724753d2f30317740ebafcc7650c
Jan Prill <Jan.Prill@mspag.com>
2020-02-02 17:25:22 +0100

    Home

--- web/js/gridsome/inviado/src/pages/Home.vue
+++ web/js/gridsome/inviado/src/pages/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <Layout>
     <main class="font-neuzeit text-white container mx-auto">
+      <!-- header -->
       <section class="lg:flex items-end">
         <section class="mt-32 text-5xl font-hairline leading-tight">
           <h1>
@@ -8,6 +9,7 @@
             much more money
             with your IT career in 
             Germany. <br />
+
             “It almost 
             feels like an illegal
             hack.”
@@ -16,10 +18,20 @@
         <section class="ml-24 italic">
           An outstanding career consists of a series of right decisions. 
           Starting with choosing the right employer at the right time. 
-          With our career due dilligince we help you to make the right 
+          With our career due diligence we help you to make the right 
           decisions and avoid the wrong ones. And here is the hack: 
           It is not you who pays for this service, but the employers in your career do.
         </section>
+      </section>
+
+      <section class="mt-64">
+        Absolutely free for candidates we do what we call "career due diligence" in which we find the perfect company for 
+        Software Engineers, regardless of them being born in Germany, coming from the EU or being Expats to strive to their 
+        full potential in the company.
+      </section>
+
+      <section class="mt-64">
+        Cards
       </section>
     </main>
   </Layout>
