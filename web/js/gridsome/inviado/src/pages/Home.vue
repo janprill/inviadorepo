3faf77a7cce4a1a011252c0c9b45b88f54ae5f11
Jan Prill <Jan.Prill@mspag.com>
2020-02-04 22:25:41 +0100

    homepage - lists

--- web/js/gridsome/inviado/src/pages/Home.vue
+++ web/js/gridsome/inviado/src/pages/Home.vue
@@ -4,15 +4,15 @@
       <!-- header -->
       <section class="lg:flex items-end">
         <section class="mt-32 text-5xl font-thin tracking-tight leading-tight">
+          <div class="text-sm uppercase">
+            welcome! inviado is your community.
+          </div>
           <h1>
-            We help you to earn 
-            much more money
-            with your IT career in 
-            Germany. <br />
-
-            “It almost 
-            feels like an illegal
-            hack.”
+            We are a community of IT 
+            talents and help each 
+            other to earn much
+            more money. It almost
+            feels like an illegal hack.
           </h1>
         </section>
         <section class="ml-24 italic">
@@ -25,9 +25,44 @@
       </section>
 
       <section class="mt-64">
-        Absolutely free for candidates we do what we call "career due diligence" in which we find the perfect company for 
-        Software Engineers, regardless of them being born in Germany, coming from the EU or being Expats to strive to their 
-        full potential in the company.
+        <h2 class="text-2xl">
+          This community earns you moeny by providing:
+        </h2>
+        <ol>
+          <li>
+            A Chat - invitation only. 
+            We arrange jobs for each other, give each other tips and earn 
+            the placement bonus that would otherwise go to recruitment agencies. Fully encrypted and highly confidential.
+          </li>
+          <li>
+            Financial figures of the most interesting companies and other tools for your "career due diligence", so that you can make the best decisions in your career.
+          </li>
+        </ol>
+      </section>
+
+      <section class="mt-64">
+        <h2 class="text-2xl">
+          How does this work exactly?
+        </h2>
+        <ol>
+          <li class="flex">
+            <div class="">
+              1
+            </div>
+            <div class="ml-3">
+              Sign up for the newsletter or contact Jan Prill (Founder of Inviado) directly (XING, LinkedIn)
+            </div>
+          </li>
+          <li>
+            You are invited to the chat if your an IT Talent and bring something to the table.
+          </li>
+          <li>
+            You yourself and every person invited by you, if placed in a great job will earn you a massive placement bonus. The reward scheme is as simple as it is ingenious and has already proven its effectiveness in the DARPA Network Challenge (Red Balloon Challenge).
+          </li>
+          <li>
+            See what you can earn and plan what you will do with the earnings.
+          </li>
+        </ol>
       </section>
 
       <section class="mt-64">
