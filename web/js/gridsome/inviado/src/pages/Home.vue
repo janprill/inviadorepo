ae2aa76b4553d688f0c42f4daf414658e03f5872
Jan Prill <Jan.Prill@mspag.com>
2020-02-05 09:57:07 +0100

    styling of the homepage

--- web/js/gridsome/inviado/src/pages/Home.vue
+++ web/js/gridsome/inviado/src/pages/Home.vue
@@ -3,9 +3,9 @@
     <main class="font-neuzeit text-white container mx-auto">
       <!-- header -->
       <section class="lg:flex items-end">
-        <section class="mt-32 text-5xl font-thin tracking-tight leading-tight">
-          <div class="text-sm uppercase">
-            welcome! inviado is your community.
+        <section class="mt-32 text-5xl font-thin leading-tight">
+          <div class="text-sm uppercase tracking-wide">
+            welcome @ inviado! Your IT-Talent community
           </div>
           <h1>
             We are a community of IT 
@@ -24,43 +24,45 @@
         </section>
       </section>
 
-      <section class="mt-64">
-        <h2 class="text-2xl">
-          This community earns you moeny by providing:
-        </h2>
-        <ol class="mt-8">
-          <NumBullet index="1">
-            A Chat - invitation only. 
-            We arrange jobs for each other, give each other tips and earn 
-            the placement bonus that would otherwise go to recruitment agencies. Fully encrypted and highly confidential.
-          </NumBullet>
-          <NumBullet index="2">
-            Financial figures of the most interesting companies and other tools for your "career due diligence", so that you can make the best decisions in your career.
-          </NumBullet>
-        </ol>
+      <section class="max-w-xl mt-32">
+        <section class="">
+          <h2 class="text-2xl">
+            This community earns you moeny by providing:
+          </h2>
+          <ol class="mt-4">
+            <NumBullet index="1">
+              A Chat - invitation only. 
+              We arrange jobs for each other, give each other tips and earn 
+              the placement bonus that would otherwise go to recruitment agencies. Fully encrypted and highly confidential.
+            </NumBullet>
+            <NumBullet index="2">
+              Financial figures of the most interesting companies and other tools for your "career due diligence", so that you can make the best decisions in your career.
+            </NumBullet>
+          </ol>
+        </section>
+
+        <section class="mt-24">
+          <h2 class="text-2xl">
+            How does this work exactly?
+          </h2>
+          <ol class="mt-4">
+            <NumBullet index="1">
+              Sign up for the newsletter or contact Jan Prill (Founder of Inviado) directly (XING, LinkedIn)
+            </NumBullet>
+            <NumBullet index="2">
+              You are invited to the chat if your an IT Talent and bring something to the table.
+            </NumBullet>
+            <NumBullet index="3">
+              You yourself and every person invited by you, if placed in a great job will earn you a massive placement bonus. The reward scheme is as simple as it is ingenious and has already proven its effectiveness in the DARPA Network Challenge (Red Balloon Challenge).
+            </NumBullet>
+            <NumBullet index="4">
+              See what you can earn and plan what you will do with the earnings.
+            </NumBullet>
+          </ol>
+        </section>
       </section>
 
-      <section class="mt-24">
-        <h2 class="text-2xl">
-          How does this work exactly?
-        </h2>
-        <ol class="mt-8">
-          <NumBullet index="1">
-            Sign up for the newsletter or contact Jan Prill (Founder of Inviado) directly (XING, LinkedIn)
-          </NumBullet>
-          <NumBullet index="2">
-            You are invited to the chat if your an IT Talent and bring something to the table.
-          </NumBullet>
-          <NumBullet index="3">
-            You yourself and every person invited by you, if placed in a great job will earn you a massive placement bonus. The reward scheme is as simple as it is ingenious and has already proven its effectiveness in the DARPA Network Challenge (Red Balloon Challenge).
-          </NumBullet>
-          <NumBullet index="4">
-            See what you can earn and plan what you will do with the earnings.
-          </NumBullet>
-        </ol>
-      </section>
-
-      <section class="mt-64">
+      <section class="mt-32">
         Cards
       </section>
     </main>
