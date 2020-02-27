a5abe41829e52c324ae202414ad216748c4d6098
Jan Prill <Jan.Prill@mspag.com>
2020-02-27 07:57:02 +0100

    use the so called purple card on the index

--- web/js/gridsome/inviado/src/pages/Index.vue
+++ web/js/gridsome/inviado/src/pages/Index.vue
@@ -4,7 +4,7 @@
       <div class="min-w-full">
         <div class="sm:mt-0 sm:w-80 sm:flex-shrink-0 lg:flex lg:flex-wrap">
           <div v-for="index in 10" :key="index" class="lg:w-1/3">
-            <Organization :index=index />
+            <PurpleCard :index=index />
           </div>
         </div>
       </div>
@@ -14,12 +14,12 @@
 
 <script>
 import OrganizationCard from '@/components/OrganizationCard'
+import PurpleCard from '@/components/PurpleCard'
 import Organization from '@/components/Organization'
 
 export default {
   components: {
-    OrganizationCard,
-    Organization
+    PurpleCard
   }
 }
 </script>
