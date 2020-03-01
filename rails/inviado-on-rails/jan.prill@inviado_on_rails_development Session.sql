758a38315f6c9100c5c37ba2a4535a93e09cc788
Jan Prill <Jan.Prill@mspag.com>
2020-03-01 15:36:10 +0100

    playing with kpi

---
+++ rails/inviado-on-rails/jan.prill@inviado_on_rails_development Session.sql
@@ -0,0 +1,69 @@
+select
+  distinct(source)
+from organizations;
+
+select
+  count(*)
+from features
+where
+  key = 'kpi';
+
+select
+  path,
+  raw
+from features
+where
+  key = 'kpi';
+
+-- software companies with gewinn_eur > 1,000,000
+SELECT
+  organizations.name,
+  features.raw ->> 'gewinn_eur'
+FROM 
+  features
+JOIN
+  featurings on (featurings.feature_id = features.id)
+JOIN
+  organizations on (featurings.featurable_id = organizations.id)
+where
+  key = 'kpi'
+  and path = '/Users/jan.prill/Downloads/software_hh_gewinn_gteq_1000000.csv'
+  and features.raw ->> 'gewinn_eur' is not null
+order by
+  REGEXP_REPLACE(features.raw ->> 'gewinn_eur', '[\,\.]', '', 'g')::float desc;
+  
+-- all current persisted organizations ordered by gewinn_eur desc
+SELECT
+  organizations.name,
+  features.path,
+  features.raw ->> 'gewinn_eur'
+FROM 
+  features
+JOIN
+  featurings on (featurings.feature_id = features.id)
+JOIN
+  organizations on (featurings.featurable_id = organizations.id)
+where
+  key = 'kpi'
+  and features.raw ->> 'gewinn_eur' is not null
+order by
+  REGEXP_REPLACE(features.raw ->> 'gewinn_eur', '[\,\.]', '', 'g')::float desc;
+
+
+
+-- all currently persisted orgs ordered by personalaufwand_pro_mitarbeiter_eur desc
+SELECT
+  organizations.name,
+  features.path,
+  features.raw ->> 'personalaufwand_pro_mitarbeiter_eur'
+FROM 
+  features
+JOIN
+  featurings on (featurings.feature_id = features.id)
+JOIN
+  organizations on (featurings.featurable_id = organizations.id)
+where
+  key = 'kpi'
+  and features.raw ->> 'personalaufwand_pro_mitarbeiter_eur' is not null
+order by
+  REGEXP_REPLACE(features.raw ->> 'personalaufwand_pro_mitarbeiter_eur', '[\,\.]', '', 'g')::float desc;
