1ed834af9c82124fad5cae83ed7a47e15036f7ea
Jan Prill <Jan.Prill@mspag.com>
2020-03-10 13:14:21 +0100

    Freelancer homepage

--- web/js/gridsome/janprill.com/src/pages/Index.vue
+++ web/js/gridsome/janprill.com/src/pages/Index.vue
@@ -5,14 +5,14 @@
         <h1 class="mt-10 text-5xl font-bold leading-none">
           I help you to get your projects on track and successfully over the finish line.
         </h1> 
-        <h2 class="mt-4 text-2xl font-bold leading-tight">
+        <h2 class="mt-10 text-2xl font-bold leading-tight">
           As an entrepreneurial thinking, extremely experienced Software Engineering Manager and Coder I work as Freelancer or Interim CTO to make your projects a complete success.
         </h2>
       </section>
       <section id="hero" class="mt-24 md:mt-48 container mx-auto">
         <div class="md:flex md:flex-row">
           <article class="bg-primary flex flex-col w-full shadow-primary mt-0 p-6 pb-32 md:pr-20 mb-auto">
-            <h1 class="max-w-lg leading-tight font-myriad font-normal text-4xl tracking-wide">
+            <h1 class="max-w-lg leading-tight font-myriad font-semibold text-4xl tracking-wide">
               Hello, I'm Jan Prill.
             </h1>
             <div class="text-lg mt-6 text-gray-400">
@@ -43,13 +43,24 @@
               </p>
               <div class="mt-8">
                 <a
-                  class="button rounded-full bg-transparent hover:bg-teal-4y-500 font-semibold hover:text-gray-300 py-2 px-4 border border-white hover:border-transparent rounded-3xl"
+                  class="button rounded-full bg-transparent hover:bg-teal-4y-500 font-semibold hover:text-gray-300 hover:bg-teal-500 py-2 px-4 border border-white hover:border-transparent rounded-3xl"
+                  target="_blank"
+                  href="mailto:jan.prill@gmail.com"
+                >
+                  Get in contact with me
+                </a>
+              </div>
+              <!--
+              <div class="mt-8">
+                <a
+                  class="button rounded-full bg-transparent hover:bg-teal-4y-500 font-semibold hover:text-gray-300 hover:bg-teal-500 py-2 px-4 border border-white hover:border-transparent rounded-3xl"
                   target="_blank"
                   href="https://keybase.io/team/inviado"
                 >
-                  Join my end-to-end encrypted chat on keybase
+                  Or chat on keybase (fully encrypted)
                 </a>
               </div>
+              -->
             </div>
           </article>
 
@@ -58,7 +69,7 @@
               <g-image src="/jan.gif" class="bg-animate rounded shadow-primary border-2 border-gray-500" />
             </div>
             <div class="text-md">
-              <h2 class="text-2xl text-center font-myriad my-6">
+              <h2 class="text-2xl text-center font-myriad my-6 font-bold">
                 I help your business to reach the next level faster.
               </h2>
               <p class="mt-4">
@@ -79,13 +90,13 @@
                 <li>SolR / Elasticsearch / ELK</li>
                 <li>PyTorch, TensorFlow, fast.ai</li>
               </ul>
-              <div class="mt-4">
+              <div class="my-6">
                 <a
-                  class="button rounded-full bg-transparent hover:bg-teal-400 font-semibold hover:text-gray-300 py-2 px-4 border border-white hover:border-transparent rounded-3xl"
+                  class="button rounded-full bg-transparent hover:bg-teal-700 font-semibold hover:text-gray-300 py-2 px-4 border border-black hover:border-transparent rounded-3xl"
                   target="_blank"
-                  href="https://keybase.io/team/inviado"
+                  href="mailto:jan.prill@gmail.com"
                 >
-                  Join my end-to-end encrypted chat on keybase
+                  Get in contact with me
                 </a>
               </div>
             </div>
@@ -96,28 +107,33 @@
       <section id="hero" class="min-h-screen container mx-auto">
         <section class="md:flex md:flex-row-reverse mt-32 w-full">
           <article class="bg-primary w-full max-w-xl shadow-primary m-0 p-6 md:p-12 md:pl-16 mb-auto">
-            <h1 class="max-w-lg leading-tight font-myriad font-light text-3xl">
+            <h1 class="max-w-lg leading-tight font-myriad font-semibold text-3xl">
               I create solutions, for which I gladly take responsibility, since I have skin in the game.
             </h1>
-            <div class="text-lg mt-6">
+            <div class="text-lg mt-6 text-gray-400">
               <p class="mt-4">
-                Without nagging, with the pure conscience of a person who gives his best, I build within a team, or alone Minimum Viable Products, start your eCommerce, or am your kick starter in the
+                Without nagging, 
+                <span class="text-gray-200 font-bold">with the pure conscience of a person who gives his best</span>
+                , I build within a team, or alone Minimum Viable Products, start your eCommerce, or am your kick starter in the
                 digitalization of your business.
               </p>
               <p class="mt-4">
-                Occasionally when an internal team is no longer properly motivated, it needs an impulse from outside. I love being that impulse. I build up islands of
-                good behavior and before your team realizes it's working at full power again. 
+                Occasionally when an internal team is no longer properly motivated, it needs an impulse from outside. I love being that impulse. 
+                <span class="text-gray-200 font-bold">I build up islands of good behavior</span>
+                and before your team realizes it's working at full power again. 
               </p>
             </div>
           </article>
 
           <article class="bg-saint flex flex-col w-full rounded-sm lg:max-w-xl shadow-primary h-auto md:mt-16 md:-mr-8 p-6 mb-auto text-base text-gray-800">
-            <h2 class="text-2xl text-center font-myriad font-light my-6">
+            <h2 class="text-2xl text-center font-myriad font-bold my-6">
               Maybe your team is missing a clear direction.
             </h2>
             <p class="mt-4">
               I value crystal clear communication. I have no problem demanding from other people what I myself am willing to give: The best performance I can get and the willingness to learn at all
-              times. In this way I encourage the desire to perform and maintain it until the goal is reached. In order to then directly strive for the next success.
+              times. In this way 
+              <span class="font-bold">I encourage the desire to perform and maintain it until the goal is reached.</span>
+              In order to then directly strive for the next success.
             </p>
             <p class="mt-4">
               Let's push things forward.
@@ -130,29 +146,33 @@
 
         <section class="md:flex md:flex-row mt-32">
           <article class="bg-primary w-full max-w-xl shadow-primary m-0 p-6 md:p-12 mb-auto">
-            <h1 class="max-w-lg font-myriad font-light text-3xl leading-tight">
+            <h1 class="max-w-lg font-myriad font-semibold text-3xl leading-tight">
               Direct, empathic integration into your team
             </h1>
-            <div class="text-lg mt-6">
+            <div class="text-lg mt-6 text-gray-400">
               <p class="mt-4">
                 As a technologically polyglot and emphatic senior software engineering manager, I can be your translator 
                 if you are not at home in the depths of software development. 
-                Because I can convey my knowledge in a language that everyone understands.
+                <span class="text-gray-200 font-bold">Because I can convey my knowledge in a language that everyone understands.</span>
               </p>
               <p class="mt-4">
-                I take the time to find out what really moves your company and your customers and find the problems as well as the opportunities. Then I go straight to work. You will be amazed at how
-                quickly I anticipate what is important, ask the right questions and how quickly success is achieved.
+                I take the time to find out what really moves your company and your customers and find the problems as well as the opportunities. 
+                <span class="text-gray-200 font-bold">Then I go straight to work.</span>
+                You will be amazed at how quickly I anticipate what is important, 
+                ask the right questions and how quickly success is achieved.
               </p>
             </div>
           </article>
 
           <article class="bg-saint flex flex-col w-full rounded-sm lg:max-w-xl shadow-primary h-auto md:mt-16 md:-ml-8 p-6 mb-auto text-base text-gray-800">
-            <h2 class="text-2xl text-center font-myriad font-light my-6">
+            <h2 class="text-2xl text-center font-myriad font-bold my-6">
               I hit the ground running, in every technology, but with special speed in DAM, PIM and MDM.
             </h2>
             <p class="mt-4">
-              I have the expertise of the IT specialist in retail. In the field of Digital Asset Management (DAM), Product Information Management (PIM) and Master Data Management (MDM) (including GS1
-              and GDSN) you will hardly find more profound know-how than with me. 
+              I have the expertise of the IT specialist in retail. In the field of 
+              <span class="font-bold">Digital Asset Management (DAM), Product Information Management (PIM) and Master Data Management (MDM) (including GS1
+              and GDSN)</span>
+              you will hardly find more profound know-how than with me. 
             </p>
             <p class="mt-4">
               From many years of experience as development manager of an integrator for Digital Asset Management, I am particularly
@@ -166,33 +186,49 @@
 
         <section class="md:flex md:flex-row-reverse mt-32">
           <article class="bg-primary w-full max-w-xl shadow-primary m-0 p-6 md:p-12 mb-auto md:pl-16">
-            <h1 class="max-w-lg font-myriad font-light text-3xl leading-tight">
+            <h1 class="max-w-lg font-myriad font-semibold text-3xl leading-tight">
               I love recruiting and speak the language of the top developers you need for your business,
             </h1>
-            <div class="text-lg mt-6">
+            <div class="text-lg mt-6 text-gray-400">
               <p class="mt-4">
-                I speak their language, not just as a hollow catch phrase, but from real, practical, sometimes hard acquired experience. 
-                The best developers notice that. I get trusted faster than even the most sophisticated recruiter. And you know how valuable that trust is, right?
+                <span class="text-gray-200 font-bold">I speak their language,</span>
+                not just as a hollow catch phrase, but 
+                <span class="text-gray-200 font-bold">from real, practical, sometimes hard acquired experience.</span>
+                The best developers notice that. I get trusted faster than even the most sophisticated recruiter. 
+                And you know how valuable that trust is, right?
               </p>
               <p class="mt-4">
-                I build up entire teams, whether in-house, remote or freelance. I have the network and the ability to build and empower entire
+                <span class="text-gray-200 font-bold">I build up entire teams, whether in-house, remote or freelance.</span>
+                I have the network and the ability to build and empower entire
                 integrated teams in your company. 
               </p>
               <p class="mt-4">
-                Alternatively, we can build a remote development team together. I can also handle the technical onboarding. You won't believe how quickly you can have a
+                <span class="text-gray-200 font-bold">Alternatively, we can build a remote development team together.</span>
+                I can also handle the technical onboarding. You won't believe how quickly you can have a
                 perfectly onboarded, integrated team in your ranks to drive your digitalization forward.
               </p>
             </div>
           </article>
 
           <article class="bg-saint flex flex-col w-full rounded-sm lg:max-w-xl shadow-primary h-auto md:mt-16 md:-mr-8 p-6 mb-auto text-base text-gray-800">
-            <h2 class="text-2xl text-center font-myriad font-light my-6">
+            <h2 class="text-2xl text-center font-myriad font-bold my-6">
               My service for IT talents
             </h2>
             <p class="mt-4">
               You are an IT talent and are in a transition in your career? Would you like to become self-employed, for example? Want to earn more money? Become a Software Engineering Manager or have
-              other ideas for a career change? Then you can benefit from my coaching. Get in touch! And we will have an absolutely no obligation, free first conversation: TODO.
+              other ideas for a career change? 
+              <span class="font-bold">Then you can benefit from my coaching. Get in touch!</span>
+              And we will have an absolutely no obligation, free first conversation:
             </p>
+              <div class="my-4">
+                <a
+                  class="button rounded-full bg-transparent hover:bg-teal-700 font-semibold hover:text-gray-300 py-2 px-4 border border-black hover:border-transparent rounded-3xl"
+                  target="_blank"
+                  href="mailto:jan.prill@gmail.com"
+                >
+                  Get in contact with me
+                </a>
+              </div>
             <div class="my-8">
               <g-image src="/career-path.png" class="bg-animate rounded shadow-primary border-2 border-gray-500" />
             </div>
